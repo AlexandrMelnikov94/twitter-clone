04868d6405bfb0b0eac9c87e8529167bc782f002
AlexandrMelnikov94 <76219280+AlexandrMelnikov94@users.noreply.github.com>
2022-01-11 20:49:17 +0300

    add post

--- twitter-clone/src/Post.js
+++ twitter-clone/src/Post.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import "./Post.css";
 import {Avatar} from "@material-ui/core";
+import Verified from "@material-ui/core";
+import {VerifiedUser} from "@material-ui/icons";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import PublishIcon from "@material-ui/icons/Publish";
 
 function Post() {
   // displayName,
@@ -12,7 +18,29 @@ function Post() {
   return (
     <div className="post">
       <div className="post__avatar">
-        <Avatar src='img/avatar.png'/>
+        <Avatar src='https://www.w3schools.com/howto/img_avatar.png'/>
+      </div>
+      <div className="post__body">
+        <div className="post__header">
+          <div className="post__headerText">
+            <h3>
+              Alexandr Melnikov <span>
+              <VerifiedUser className="post__badge"/> @AlexandrMelnikov
+            </span>
+            </h3>
+          </div>
+          <div className="post__headerDescription">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab esse quasi quisquam.</p>
+          </div>
+        </div>
+        <img src="https://i0.wp.com/www.alphr.com/wp-content/uploads/2017/11/How-to-post-a-Live-Photo-on-Instagram1.jpg?fit=900%2C600&ssl=1"
+             alt="post"/>
+        <div className="post__footer">
+          <ChatBubbleOutlineIcon fontSize="small"/>
+          <RepeatIcon fontSize="small"/>
+          <FavoriteBorderIcon fontSize='small'/>
+          <PublishIcon fontSize='small'/>
+        </div>
       </div>
     </div>
   )

bdb54db02c5df7431bd884a8169476f8fd81c8a6
AlexandrMelnikov94 <76219280+AlexandrMelnikov94@users.noreply.github.com>
2022-01-12 13:56:14 +0300

    add widgets

--- twitter-clone/src/TweetBox.js
+++ twitter-clone/src/TweetBox.js
@@ -11,9 +11,9 @@ function TweetBox() {
           <input placeholder="What's happening?"
                  type="text"/>
         </div>
-        <input className="tweetBox__imageInput"
-               placeholder="Optional: Enter image URL"
-               type="text"/>
+        {/*<input className="tweetBox__imageInput"*/}
+        {/*       placeholder="Optional: Enter image URL"*/}
+        {/*       type="text"/>*/}
         <Button className="tweetBox__tweetButton">Tweet</Button>
       </form>
     </div>
--- twitter-clone/src/Widgets.js
+++ twitter-clone/src/Widgets.js
@@ -1,10 +1,27 @@
 import React, {Component} from 'react';
 import './Widgets.css';
+import {
+  TwitterTimelineEmbed,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
   return (
-    <div className='widgets'>
-      <h2>Widgets</h2>
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon"/>
+        <input placeholder="Search Twitter"
+               type="text"/>
+      </div>
+      <div className="widgets__widgetsContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={"1481116331680440324"}/>
+        <TwitterTimelineEmbed sourceType="profile"
+                              screenName="elonmusk"
+                              options={{height: 400}}/>
+      </div>
     </div>
   )
 }
